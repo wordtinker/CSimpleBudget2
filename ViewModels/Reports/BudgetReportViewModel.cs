5739cc8d35c1d1c52548f169050d1fb0f5bf9ba6
wordtinker <wordtinker@yandex.ru>
2018-01-19 11:43:17 +0300

    Take 78

--- ViewModels/Reports/BudgetReportViewModel.cs
+++ ViewModels/Reports/BudgetReportViewModel.cs
@@ -1,7 +1,32 @@
-﻿namespace ViewModels.Reports
+﻿using Models.Interfaces;
+using System.Collections.ObjectModel;
+using ViewModels.Elements;
+
+namespace ViewModels.Reports
 {
     public class BudgetReportViewModel
     {
-        // TODO vm + xaml
+        private IDataProvider dataProvider;
+
+        public MonthYearSelector Selector { get; }
+        public ObservableCollection<BudgetBar> Bars { get; }
+
+        public BudgetReportViewModel(IDataProvider dataProvider)
+        {
+            this.dataProvider = dataProvider;
+            Bars = new ObservableCollection<BudgetBar>();
+            Selector = new MonthYearSelector(dataProvider, -0, +0);
+            Selector.PropertyChanged += (sender, e) => UpdateBars();
+            UpdateBars();
+        }
+
+        private void UpdateBars()
+        {
+            Bars.Clear();
+            foreach (var item in dataProvider.GetSpendings(Selector.SelectedYear, Selector.SelectedMonth))
+            {
+                Bars.Add(new BudgetBar(item));
+            }
+        }
     }
 }
